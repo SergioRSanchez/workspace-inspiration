2fd2f08712d87076e7ee2c310f0161b02a332f18
Sergio Sanchez <ssanchezfilho@gmail.com>
2023-10-10 18:20:03 -0300

    feat: create repository with full code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,143 @@
+'use client'
+
+import { useState } from 'react'
+import * as Yup from 'yup';
 import Image from 'next/image'
 
+import { ChevronRight } from 'lucide-react'
+
+import bg from '@/assets/bg.png'
+import rocket from '@/assets/rocket.svg'
+import lightning from '@/assets/lightning.svg'
+import tools from '@/assets/tools.svg'
+import gift from '@/assets/gift.svg'
+
+
 export default function Home() {
+  const [email, setEmail] = useState<string>('')
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Digite um endereço de e-mail válido')
+      .required('O campo de e-mail é obrigatório'),
+  });
+
+  function handleSubmit(e: any) {
+    e.preventDefault()
+
+    if (!validationSchema.isValidSync({ email })) {
+      alert('Entre com um e-mail válido')
+      return
+    }
+
+    alert(`Obrigado por se inscrever! Receba nossa newsletter em ${email}`)
+    setEmail('')
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className='bg-[#09090A] min-h-screen relative flex flex-col overflow-x-hidden'>
+      <Image 
+        alt='picture of some workspaces'
+        src={bg}
+        className='absolute h-[52rem] w-[52rem] -right-[4rem] opacity-50 lg:opacity-100'
+      />
+       
+      <div className='py-10 sm:pt-40 sm:pb-20 sm:px-28 px-3 z-10 flex flex-col gap-40'>
+        {/* CONTENT */}
+        <div className='flex flex-col gap-8'>
+          <div className='flex flex-col gap-4'>
+            <h2 className='uppercase font-medium leading-6 tracking-widest'>Newsletter exclusiva</h2>
+            <h1 className='text-5xl font-bold leading-[3rem] text-white'>Workspace inspiration</h1>
+          </div>
+
+          
+          <p className='text-[#C4C4CC] sm:w-[27.8125rem] text-xl leading-7'>Assine nossa newsletter e transforme seu espaço de trabalho em um oásis de produtividade!</p>
+          
+          <form 
+            className='flex gap-2'
+            onSubmit={handleSubmit}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            <input 
+              type="text" 
+              placeholder='Digite seu e-mail'
+              className='px-2 sm:px-3 py-3 sm:py-4 bg-[#202024] rounded-md sm:w-[18.1875rem] text-white placeholder:text-[#C4C4CC] active:ring-[1px] active:ring-[#996DFF] border-none focus:ring-[1px] focus:ring-[#996DFF] outline-0 transition-all duration-300'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-          </a>
+            <button 
+              type="submit"
+              className='uppercase text-black p-3 sm:p-4 font-bold text-sm bg-gradient-to-r from-[#996DFF] to-[#C7AFFF] flex gap-2 self-stretch items-center text-center rounded hover:brightness-110 transition-all duration-300 hover:shadow-[0_5px_60px_-15px_rgba(255,255,255,0.3)] disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:shadow-none disabled:hover:opacity-50 disabled:hover:brightness-100'
+              disabled={!email}
+            >
+              Quero receber
+              <ChevronRight className='h-5 w-5 text-black'/>
+            </button>
+          </form>
+        </div>
+
+        {/* CARDS */}
+        <div className='flex flex-col sm:flex-row gap-10 justify-between'>
+
+         {/* CARD */}
+         <div className='sm:w-[17.125rem] flex flex-col gap-2'>
+          <div className='flex flex-col gap-4'>
+            <Image 
+              src={rocket}
+              alt='rocket'
+              className=''
+            />
+
+            <h3 className='text-lg text-white font-bold leading-7'>As últimas inovações</h3>
+          </div>
+
+          <p className='text-[#C4C4CC] text-sm leading-5'>Mantenha-se atualizado com as mais recentes tecnologias e acessórios que vão revolucionar a forma como você trabalha.</p>
+         </div>
+         
+         <div className='sm:w-[17.125rem] flex flex-col gap-2'>
+          <div className='flex flex-col gap-4'>
+            <Image 
+              src={lightning}
+              alt='lightning'
+              className=''
+            />
+
+            <h3 className='text-lg text-white font-bold leading-7'>Reviews detalhadas</h3>
+          </div>
+
+          <p className='text-[#C4C4CC] text-sm leading-5'>Analisamos os produtos mais populares do mercado para que você possa fazer as escolhas certas.</p>
+         </div>
+         
+         <div className='sm:w-[17.125rem] flex flex-col gap-2'>
+          <div className='flex flex-col gap-4'>
+            <Image 
+              src={tools}
+              alt='tools'
+              className=''
+            />
+
+            <h3 className='text-lg text-white font-bold leading-7'>Dicas de organização</h3>
+          </div>
+
+          <p className='text-[#C4C4CC] text-sm leading-5'>Aumente sua eficiência e proteja sua saúde com conselhos especializados sobre ergonomia e organização.</p>
+         </div>
+         
+         <div className='sm:w-[17.125rem] flex flex-col gap-2'>
+          <div className='flex flex-col gap-4'>
+            <Image 
+              src={gift}
+              alt='gift'
+              className=''
+            />
+
+            <h3 className='text-lg text-white font-bold leading-7'>Ofertas exclusivas</h3>
+          </div>
+
+          <p className='text-[#C4C4CC] text-sm leading-5'>Como membro da nossa comunidade, você terá acesso a descontos e promoções especiais em produtos selecionados.</p>
+         </div>
+
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
